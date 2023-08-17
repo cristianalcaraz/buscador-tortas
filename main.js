4fa79e409d263496c3983d53521861041c303dba
unknown <cristianalcaraz@outlook.com>
2023-08-16 21:25:29 -0300

    arreglo de errores

--- main.js
+++ main.js
@@ -3,38 +3,50 @@
 //arregloq ue va a contener los productos creados
 let variedades = [];
 
+class gustos {
+    constructor(nombre, precio) {
+      this.nombre = nombre;
+      this.precio = precio;
+      this.vendido = false;
+    }
+  
+    vender() {
+      this.vendido = true;
+    }
+  }
+  
 //agrego variedades  creados por la clase gusto al arreglo
 variedades.push(new gustos("Selva Negra", 1000));
-variedades.push(new gustos("Lemon pai", 750));
+variedades.push(new gustos("Lemon pie", 750));
 variedades.push(new gustos("Frutales", 720));
 variedades.push(new gustos("Budines", 100));
+variedades.push(new gustos("Torta helada", 2000));
 
 //pedimos el nombre de la torta  a consultar
-let nombre = prompt("Ingrese el nombre de la torta  consultar");
+let nombre = prompt("Ingrese el nombre de la torta  consultar. Escriba ESC para salir");
 
 while (nombre != "ESC") {
     //declaramos la variable que contendrá variedades en caso de encontrarlo
-    let variedades;
+    let gusto;
 
     //recorremos el arreglo verificando si alguno de los gustos cumple con el nombre
-    for (const item of gustos) {
-        if (item.nombre === nombre) {
-            gustos = item;
+    for (const item of variedades) {
+        if (item.nombre.toLowerCase() === nombre.toLowerCase()) {
+            gusto = item;
         }
     }
 
-    if (gustos) {
+    if (gusto) {
         let mensaje = `
-      Nombre: ${gustos.nombre}
-      Precio: $${gustos.precio}
+      Nombre: ${gusto.nombre}
+      Precio: $${gusto.precio}
     `;
-
         alert(mensaje);
     } else {
         alert("No se encuentra disponible");
     }
 
-    nombre = prompt("Ingrese el nombre de la torta   consultar");
+    nombre = prompt("Ingrese el nombre de la torta consultar.  Escriba ESC para salir");
 }
 
 
@@ -42,18 +54,18 @@ while (nombre != "ESC") {
 
 const productos = [
     { id: 1, nombre: "tarta", precio: 1000 },
-    { id: 2, nombre: "Lemon pei", precio: 750 },
+    { id: 2, nombre: "Lemon pie", precio: 750 },
     { id: 3, nombre: "Budin", precio: 1200 },
     { id: 4, nombre: "Pan Dulce", precio: 375 },
 ];
 
 
-const filtrados = productos.filter((item) => item.precio > 500);
+let filtrados = productos.filter((item) => item.precio > 500);
 console.log(filtrados);
 
-let precio = parseInt(prompt("Ingrese el precio minimo"));
+let precio = parseInt(prompt("Ingrese el precio minimo para buscar ofertas"));
 
-const filtrados = productos.filter((item) => item.precio > precio);
+filtrados = productos.filter((item) => item.precio < precio);
 
 filtrados.forEach((item) => {
     let mensaje = `
@@ -61,5 +73,7 @@ filtrados.forEach((item) => {
      Nombre: ${item.nombre}
      Precio: $${item.precio}
    `;
-}
-alert(mensaje);
+   alert(mensaje);
+});
+
+
